a1b407c833255ef90b848b6d0adaec3555d9f22f
maks-makentosh <mrkoren3@mail.ru>
2020-04-20 21:55:39 +0300

    All

---
+++ script.js
@@ -0,0 +1,86 @@
+'use strict'
+
+var btn1 = document.getElementById('btn_1');
+var item1 = document.getElementById('item_1');
+var cl1 = document.getElementById('cl1');
+btn1.onclick = function(){
+    item1.style.background = '#70f5ea';
+    btn1.style.display = 'none';
+    cl1.style.display = 'block';
+    cl1.style.color  = '#000';
+}
+cl1.onclick = function(){
+    item1.style.background = 'linear-gradient(to right, rgba(0,0,0,0.65) 0%, rgba(0,0,0,0.65) 100%), url(img/service/service_1.png)';
+    btn1.style.display = 'block';
+    cl1.style.display = 'none';
+}
+var btn2 = document.getElementById('btn_2');
+var item2 = document.getElementById('item_2');
+var cl2 = document.getElementById('cl2');
+btn2.onclick = function(){
+    item2.style.background = '#70f5ea';
+    btn2.style.display = 'none';
+    cl2.style.display = 'block';
+    cl2.style.color  = '#000';
+}
+cl2.onclick = function(){
+    item2.style.background = 'linear-gradient(to right, rgba(0,0,0,0.65) 0%, rgba(0,0,0,0.65) 100%), url(img/service/service_2.png)';
+    btn2.style.display = 'block';
+    cl2.style.display = 'none';
+}
+var btn3 = document.getElementById('btn_3');
+var item3 = document.getElementById('item_3');
+var cl3 = document.getElementById('cl3');
+btn3.onclick = function(){
+    item3.style.background = '#70f5ea';
+    btn3.style.display = 'none';
+    cl3.style.display = 'block';
+    cl3.style.color  = '#000';
+}
+cl3.onclick = function(){
+    item3.style.background = 'linear-gradient(to right, rgba(0,0,0,0.65) 0%, rgba(0,0,0,0.65) 100%), url(img/service/service_3.png)';
+    btn3.style.display = 'block';
+    cl3.style.display = 'none';
+}
+var btn4 = document.getElementById('btn_4');
+var item4 = document.getElementById('item_4');
+var cl4 = document.getElementById('cl4');
+btn4.onclick = function(){
+    item4.style.background = '#70f5ea';
+    btn4.style.display = 'none';
+    cl4.style.display = 'block';
+    cl4.style.color  = '#000';
+}
+cl4.onclick = function(){
+    item4.style.background = 'linear-gradient(to right, rgba(0,0,0,0.65) 0%, rgba(0,0,0,0.65) 100%), url(img/service/service_4.png)';
+    btn4.style.display = 'block';
+    cl4.style.display = 'none';
+}
+var btn5 = document.getElementById('btn_5');
+var item5 = document.getElementById('item_5');
+var cl5 = document.getElementById('cl5');
+btn5.onclick = function(){
+    item5.style.background = '#70f5ea';
+    btn5.style.display = 'none';
+    cl5.style.display = 'block';
+    cl5.style.color  = '#000';
+}
+cl5.onclick = function(){
+    item5.style.background = 'linear-gradient(to right, rgba(0,0,0,0.65) 0%, rgba(0,0,0,0.65) 100%), url(img/service/service_5.png)';
+    btn5.style.display = 'block';
+    cl5.style.display = 'none';
+}
+var btn6 = document.getElementById('btn_6');
+var item6 = document.getElementById('item_6');
+var cl6 = document.getElementById('cl6');
+btn6.onclick = function(){
+    item6.style.background = '#70f5ea';
+    btn6.style.display = 'none';
+    cl6.style.display = 'block';
+    cl6.style.color  = '#000';
+}
+cl6.onclick = function(){
+    item6.style.background = 'linear-gradient(to right, rgba(0,0,0,0.65) 0%, rgba(0,0,0,0.65) 100%), url(img/service/service_6.png)';
+    btn6.style.display = 'block';
+    cl6.style.display = 'none';
+}
